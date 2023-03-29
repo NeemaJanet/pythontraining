c7df046ac5ee4bdb2d36825aaff69eade09d051e
NeemaJanet <neemajanet100@gmail.com>
2023-03-29 10:55:35 +0300

    first commit

---
+++ mylargest.py
@@ -0,0 +1,10 @@
+mynum=input("Enter 3 numbers: ")
+mynumb=input("Enter another number: ")
+
+if (mynum>mynumb):
+ print(mynum)
+elif(mynumb>mynum):
+ print(mynumb)
+else:
+ print(mynum + mynumb)
+       
---
+++ mystrings.py
@@ -0,0 +1,8 @@
+first_name_one="HaNNais"
+print(first_name_one.strip())
+print(first_name_one)
+msg="""Dear Terry,,
+You must cut down the mightiest 
+tree in the forest with…
+a herring! <3"""
+print(msg)
