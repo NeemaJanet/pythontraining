e2b440b98e21f5def98b62a41d5b94df019461ee
NeemaJanet <123103378+NeemaJanet@users.noreply.github.com>
2023-03-29 16:27:38 +0300

    task

---
+++ task2.py
@@ -0,0 +1,9 @@
+num=float(input("Enter a number: "))
+numb= num%2
+numbe=num%4
+if (numb==0 and numbe==0):
+ print("Multiple of 4")
+elif(numb==1 and numbe==0):
+ print("Odd")
+else:(numb==0 and numbe==0)
+print("Even")
